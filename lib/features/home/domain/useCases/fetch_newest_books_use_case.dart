45d199cc6cb5298f288325a75e0996fb444b2f6f
mohamedAdel <ma1704417@gmail.com>
2024-09-14 13:47:26 +0300

    make generic use case and finish use cases

--- lib/features/home/domain/useCases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/useCases/fetch_newest_books_use_case.dart
@@ -4,13 +4,13 @@ import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:bookly_app/features/home/domain/useCases/use_case_no_param.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>>{
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>>{
   final HomeRepo homeRepo;
-  FetchFeaturedBooksUseCase(this.homeRepo);
+  FetchNewestBooksUseCase(this.homeRepo);
 
   @override
   Future<Either<Failure, List<BookEntity>>> call() async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchNewestBooks();
   }
 
 
