ff7d7a04ad54b1d1b720133654194014cfad3348
mohamedAdel <ma1704417@gmail.com>
2024-10-06 12:34:55 +0300

    cache data of books locally by hive

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,46 +1,47 @@
+import 'package:bookly_app/core/functions/save_books_data.dart';
 import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/core/utils/constants.dart';
 import 'package:bookly_app/features/home/data/models/book_model/BookModel.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
-  Future<List<BookEntity>> fetchFeaturedBooks ();
-  Future<List<BookEntity>> fetchNewestBooks ();
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
-
 class HomeRemoteDataSourceImpel extends HomeRemoteDataSource {
-
-  final ApiService apiService ;
+  final ApiService apiService;
 
   HomeRemoteDataSourceImpel(this.apiService);
 
   @override
   Future<List<BookEntity>> fetchFeaturedBooks() async {
-    var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&q=programming");
+    var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&q=programming");
 
     List<BookEntity> books = getBooksList(data);
 
+    saveBooksData(books, kFeaturedBox);
+
     return books;
   }
-
 
   @override
   Future<List<BookEntity>> fetchNewestBooks() async {
-    var data = await apiService.get(endPoint: "volumes?q=programming&Filtering=free-ebooks&Sorting=newest");
+    var data = await apiService.get(
+        endPoint: "volumes?q=programming&Filtering=free-ebooks&Sorting=newest");
 
     List<BookEntity> books = getBooksList(data);
 
     return books;
   }
-
 
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
     List<BookEntity> books = [];
 
-    for (var bookMap in data["items"]){
+    for (var bookMap in data["items"]) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
   runApp(const Bookly());
+
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
@@ -13,8 +18,7 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark()
-          .copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       routerConfig: AppRouter.router,
     );
   }
